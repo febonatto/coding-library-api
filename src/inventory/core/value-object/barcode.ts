bd330dd8f7ca1f926ba56728653128ff8cd28c8a
febonatto <fernando.bonatto@dotlib.com>
2025-12-26 13:56:00 -0300

    feat: Criado value object Barcode para controlar a criação de códigos de barra válidos para a aplicação

---
+++ src/inventory/core/value-object/barcode.ts
@@ -0,0 +1,41 @@
+import { DomainError } from 'src/shared/core/errors/domain.error';
+
+interface BarcodeConstructorProps {
+  acquisitionYear: number;
+  sequence: number;
+}
+
+export class Barcode {
+  private readonly foundationYear = 2000;
+  private readonly start = 'SLB';
+  private _value: string;
+
+  constructor({ acquisitionYear, sequence }: BarcodeConstructorProps) {
+    if (acquisitionYear < this.foundationYear) {
+      throw new DomainError(
+        `Acquisition year must be ${this.foundationYear} or later`,
+      );
+    }
+    const formattedSequence = this.transformSequence(sequence);
+    const verificationDigit =
+      this.calculateVerificationDigit(formattedSequence);
+    this._value = `${this.start}-${acquisitionYear}-${formattedSequence}-${verificationDigit}`;
+  }
+
+  get value(): string {
+    return this._value;
+  }
+
+  private transformSequence(sequence: number): string {
+    return sequence.toString().padStart(6, '0');
+  }
+
+  private calculateVerificationDigit(sequence: string): number {
+    const digits = sequence.split('').map((digit) => parseInt(digit, 10));
+    const sum = digits.reduce(
+      (acc, digit, index) => acc + digit * (6 - index),
+      0,
+    );
+    return sum % 10;
+  }
+}
